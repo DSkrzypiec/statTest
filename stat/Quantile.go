f56c20c2dcbd432d1ecb754b0b5bf1a841b2ba2a
DSkrzypiec <ds320335@students.mimuw.edu.pl>
2019-06-22 15:38:42 +0200

    Minor fix in Quantile. Add Quantiles function and add tests from calculating quantiles in R.

--- stat/Quantile.go
+++ stat/Quantile.go
@@ -22,9 +22,13 @@ func Quantile(x []float64, prob float64) float64 {
 	if prob < 0.0 || prob > 1.0 {
 		return math.NaN()
 	}
-
 	newX := sort.Float64Slice(x)
 	newX.Sort()
+
+	if prob == 1.0 {
+		return newX[len(newX)-1]
+	}
+
 	j := qj(x, prob)
 	gpVal := gp(x, prob)
 	quantile := (1.0-gpVal)*newX[j] + gpVal*newX[j+1]
@@ -32,11 +36,39 @@ func Quantile(x []float64, prob float64) float64 {
 	return quantile
 }
 
+// Quantiles does exactly the same as Quantile but calculates possible many
+// quantilies for each given probability in argument probs. The same result
+// can be obtained by calling Quantile several times. Difference is that in
+// this implementation x slice is sorted only one time, thus for large x it
+// can be helpful in term of performance.
+func Quantiles(x, probs []float64) []float64 {
+	quantiles := make([]float64, len(probs))
+	newX := sort.Float64Slice(x)
+	newX.Sort()
+
+	for i, prob := range probs {
+		if prob < 0.0 || prob > 1.0 {
+			quantiles[i] = math.NaN()
+			continue
+		}
+		if prob == 1.0 {
+			quantiles[i] = newX[len(newX)-1]
+			continue
+		}
+
+		j := qj(x, prob)
+		gpVal := gp(x, prob)
+		quantiles[i] = (1.0-gpVal)*newX[j] + gpVal*newX[j+1]
+	}
+
+	return quantiles
+}
+
 // Function qj calculates "j" index in order statistics for Quantile function.
 func qj(x []float64, prob float64) int {
 	xl := float64(len(x))
 	stat := prob*(xl-1.0) + 1.0
-	return int(math.Floor(stat))
+	return int(math.Floor(stat)) - 1 // -1 because go is 0-index
 }
 
 // Function gp calculates value of g(p) function for Quantile function.
---
+++ stat/Quantile_test.go
@@ -0,0 +1,120 @@
+package stat
+
+import (
+	"fmt"
+	"math"
+	"testing"
+)
+
+func TestUnitQuantileBasic(t *testing.T) {
+	x, rResults := quantileResultsFromR1()
+	x2, rResults2 := quantileResultsFromR2()
+
+	for p, rQuant := range rResults {
+		goQuant := Quantile(x, p)
+
+		if !closeEnough(rQuant, goQuant, 0.001) {
+			t.Errorf("Incorrect quantile for p = %f. Expected: %f, got: %f \n",
+				p, rQuant, goQuant)
+		}
+	}
+
+	for p, rQuant := range rResults2 {
+		goQuant := Quantile(x2, p)
+
+		if !closeEnough(rQuant, goQuant, 0.001) {
+			t.Errorf("Incorrect quantile for p = %f. Expected: %f, got: %f \n",
+				p, rQuant, goQuant)
+		}
+	}
+
+	if !math.IsNaN(Quantile(x, 2.5)) {
+		t.Errorf("Expected NaN, got: %f \n", Quantile(x, 2.5))
+	}
+
+	if !math.IsNaN(Quantile(x, -10.0)) {
+		t.Errorf("Expected NaN, got: %f \n", Quantile(x, 2.5))
+	}
+}
+
+// This unit test tests if Quantiles function is consistent with Quantile function.
+func TestUnitQuantiles(t *testing.T) {
+	epsilon := 0.0001
+	input := []float64{14.54, 20.12, 50.0, 211.0, -10.5, 3.14159}
+	probs := []float64{0.0, 1.0, -19.0, 44.123, 0.04, 0.000123, 0.995, 0.55, 0.75, 0.88}
+
+	expected := make([]float64, len(probs))
+	for i, p := range probs {
+		expected[i] = Quantile(input, p)
+	}
+	result := Quantiles(input, probs)
+
+	for i, res := range result {
+		if !closeEnough(res, expected[i], epsilon) {
+			t.Errorf("Quantile vs Quantiles failed for %d: Expected: %f, got: %f\n",
+				i, expected[i], res)
+		}
+	}
+}
+
+func BenchmarkQuantile(b *testing.B) {
+	input, _ := quantileResultsFromR2()
+	var q float64
+
+	for i := 0; i < b.N; i++ {
+		q = Quantile(input, 0.90)
+	}
+	fmt.Sprintf("%f", q)
+}
+
+// Function quantileResultsFromR1 returns x slice and its quantiles calculated
+// in R using quantile().
+func quantileResultsFromR1() ([]float64, map[float64]float64) {
+	input := []float64{14.54, 20.12, 50.0, 211.0, -10.5, 3.14159}
+	res := make(map[float64]float64)
+	res[0.0] = -10.50
+	res[0.05] = -7.08960
+	res[0.15] = -0.2688075
+	res[0.55] = 18.7250
+	res[0.75] = 42.530
+	res[0.90] = 130.500
+	res[0.99] = 202.950
+	res[1.00] = 211.0
+
+	return input, res
+}
+
+func quantileResultsFromR2() ([]float64, map[float64]float64) {
+	input := make([]float64, 1000)
+	for i := 0; i < 1000; i++ {
+		input[i] = float64(i + 1)
+	}
+
+	res := make(map[float64]float64)
+	res[0.0] = 1.0
+	res[0.05] = 50.95
+	res[0.15] = 150.85
+	res[0.55] = 550.45
+	res[0.75] = 750.25
+	res[0.90] = 900.10
+	res[0.99] = 990.01
+	res[1.00] = 1000.0
+
+	return input, res
+}
+
+func closeEnough(x, y, epsilon float64) bool {
+	if math.IsNaN(x) && math.IsNaN(y) {
+		return true
+	}
+	if math.IsInf(x, 1) && math.IsInf(y, 1) {
+		return true
+	}
+	if math.IsInf(x, -1) && math.IsInf(y, -1) {
+		return true
+	}
+	if math.Abs(x-y) < epsilon {
+		return true
+	}
+	return false
+}
